e1f0817c738ff26297f00a2a02a43b0221eeecc0
Igor Oliveira <lrmcpe22@gmail.com>
2022-06-17 23:04:02 -0300

    criando e estilizando a seção de tecnologias

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+/*
+var variavelHtml = document.querySelector(".habilidades-titulo");
+variavelHtml.addEventListener("mousemove", function(){
+
+    //variavelHtml.style.display = "none";
+
+
+    variavelHtml.style.color = "red";
+    
+});
+*/
